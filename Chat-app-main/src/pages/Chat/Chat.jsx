99178b96b2fbb5f435e9fa64feff42882f0c8885
Vasu <vasusingh1305@gmail.com>
2024-09-08 14:17:35 +0530

    left

--- Chat-app-main/src/pages/Chat/Chat.jsx
+++ Chat-app-main/src/pages/Chat/Chat.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './Chat.css'
-import LeftSidebar from '../../components/LeftsideBar/LeftSideBar'
+import LeftSidebar from '../../components/LeftsideBar/LeftSideBar.jsx'
 import ChatBox from '../../components/ChatBox/ChatBox'
 import RightSidebar from '../../components/RightSideBar/RightSideBar'
 import { AppContext } from '../../context/AppContext'
